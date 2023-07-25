e18cb8e7cfbbb70788482d2a9cfde443b728c651
John Keen <blueisgoodtoo@gmail.com>
2023-07-25 16:29:48 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from 'react';
+import Card from './Card';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div>
+            <h1>Music I like</h1>
+            <Card
+                albumTitle=""
+                albumDescription="Rocket to Russia is the third studio album by the American punk rock band Ramones, and was released on November 4, 1977, through Sire Records. Its origins date back to the summer of 1977, when 'Sheena Is a Punk Rocker' was released as a single. That summer was known as the peak of the punk rock genre since many punk bands were offered recording contracts. The album's recording began in August 1977, and the band had a considerably larger budget with Sire allowing them between $25,000 and $30,000; much of this money went toward the album's production rather than recording."
+                imageURL="https://upload.wikimedia.org/wikipedia/en/5/50/Ramones_-_Rocket_to_Russia_cover.jpg"
+                buttonText="OK"/>
+            <Card
+                albumTitle=""
+                albumDescription="Rocket to Russia is the third studio album by the American punk rock band Ramones, and was released on November 4, 1977, through Sire Records. Its origins date back to the summer of 1977, when 'Sheena Is a Punk Rocker' was released as a single. That summer was known as the peak of the punk rock genre since many punk bands were offered recording contracts. The album's recording began in August 1977, and the band had a considerably larger budget with Sire allowing them between $25,000 and $30,000; much of this money went toward the album's production rather than recording."
+                imageURL="https://upload.wikimedia.org/wikipedia/en/5/50/Ramones_-_Rocket_to_Russia_cover.jpg"
+                buttonText="OK"/>
+            <Card
+                albumTitle=""
+                albumDescription="Rocket to Russia is the third studio album by the American punk rock band Ramones, and was released on November 4, 1977, through Sire Records. Its origins date back to the summer of 1977, when 'Sheena Is a Punk Rocker' was released as a single. That summer was known as the peak of the punk rock genre since many punk bands were offered recording contracts. The album's recording began in August 1977, and the band had a considerably larger budget with Sire allowing them between $25,000 and $30,000; much of this money went toward the album's production rather than recording."
+                imageURL="https://upload.wikimedia.org/wikipedia/en/5/50/Ramones_-_Rocket_to_Russia_cover.jpg"
+                buttonText="OK"/>
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = (props) => {
+    return (
+        <div className="card" style={{width: '18rem'}}>
+            <img src={props.imageURL} className="card-img-top" alt="Album" />
+            <div className="card-body">
+                <h5 className="card-title">{props.albumTitle}</h5>
+                <p className="card-text">{props.albumDescription}</p>
+                <button href="#" className="btn btn-primary">
+                    {props.buttonText}
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
