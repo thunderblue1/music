40a15f32d62adb3e4473f853760268bb0f39e697
John Keen <blueisgoodtoo@gmail.com>
2023-07-26 11:19:53 -0700

    Stopping point two

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import { React } from 'react';
+import React, { useState } from 'react';
 import Card from './Card';
+import './App.css';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Music I like</h1>
-            <Card
-                albumTitle=""
-                albumDescription="Rocket to Russia is the third studio album by the American punk rock band Ramones, and was released on November 4, 1977, through Sire Records. Its origins date back to the summer of 1977, when 'Sheena Is a Punk Rocker' was released as a single. That summer was known as the peak of the punk rock genre since many punk bands were offered recording contracts. The album's recording began in August 1977, and the band had a considerably larger budget with Sire allowing them between $25,000 and $30,000; much of this money went toward the album's production rather than recording."
-                imageURL="https://upload.wikimedia.org/wikipedia/en/5/50/Ramones_-_Rocket_to_Russia_cover.jpg"
-                buttonText="OK"/>
-            <Card
-                albumTitle=""
-                albumDescription="Rocket to Russia is the third studio album by the American punk rock band Ramones, and was released on November 4, 1977, through Sire Records. Its origins date back to the summer of 1977, when 'Sheena Is a Punk Rocker' was released as a single. That summer was known as the peak of the punk rock genre since many punk bands were offered recording contracts. The album's recording began in August 1977, and the band had a considerably larger budget with Sire allowing them between $25,000 and $30,000; much of this money went toward the album's production rather than recording."
-                imageURL="https://upload.wikimedia.org/wikipedia/en/5/50/Ramones_-_Rocket_to_Russia_cover.jpg"
-                buttonText="OK"/>
-            <Card
-                albumTitle=""
-                albumDescription="Rocket to Russia is the third studio album by the American punk rock band Ramones, and was released on November 4, 1977, through Sire Records. Its origins date back to the summer of 1977, when 'Sheena Is a Punk Rocker' was released as a single. That summer was known as the peak of the punk rock genre since many punk bands were offered recording contracts. The album's recording began in August 1977, and the band had a considerably larger budget with Sire allowing them between $25,000 and $30,000; much of this money went toward the album's production rather than recording."
-                imageURL="https://upload.wikimedia.org/wikipedia/en/5/50/Ramones_-_Rocket_to_Russia_cover.jpg"
-                buttonText="OK"/>
-        </div>
-    )
-}
+  const [albumList, setAlbumList] = useState([
+    {
+      artistId: 0,
+      artist: 'The Beatles',
+      title: 'Yellow Submarine',
+      description:
+        'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
+      year: 1969,
+      image:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/TheBeatles-YellowSubmarinealbumcover.jpg/220px-TheBeatles-YellowSubmarinealbumcover.jpg',
+    },
+    {
+      artistId: 1,
+      artist: 'The Beatles',
+      title: 'Abbey Road',
+      description:
+        'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
+      year: 1969,
+      image:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Beatles_-_Abbey_Road.jpg/220px-Beatles_-_Abbey_Road.jpg',
+    },
+    {
+      artistId: 2,
+      artist: 'The Beatles',
+      title: 'Let It Be',
+      description:
+        "Let It Be is the twelfth and final studio album by the English rock band the Beatles. It was released on 8 May 1970, almost a month after the group's break-up.",
+      year: 1970,
+      image:
+        'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/LetItBe.jpg/220px-LetItBe.jpg',
+    },
+  ]);
+
+  const renderedList = () => {
+    return albumList.map((album) => {
+      return (
+        <Card
+          albumTitle={album.title}
+          albumDescription={album.description}
+          buttonText='OK'
+          imageURL={album.image}
+        />
+      );
+    });
+  };
+
+  return <div className='container'>{renderedList()}</div>;
+};
 
 export default App;
