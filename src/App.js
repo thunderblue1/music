d06bcaab92f756ebab9ba676ad380499c73a1f93
John Keen <blueisgoodtoo@gmail.com>
2023-08-02 10:03:53 -0700

    Stopping Point #3

--- src/App.js
+++ src/App.js
@@ -1,55 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import './App.css';
+import SearchForm from './SearchForm';
+import dataSource from './dataSource';
 
-const App = () => {
-  const [albumList, setAlbumList] = useState([
-    {
-      artistId: 0,
-      artist: 'The Beatles',
-      title: 'Yellow Submarine',
-      description:
-        'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
-      year: 1969,
-      image:
-        'https://upload.wikimedia.org/wikipedia/en/thumb/a/ac/TheBeatles-YellowSubmarinealbumcover.jpg/220px-TheBeatles-YellowSubmarinealbumcover.jpg',
-    },
-    {
-      artistId: 1,
-      artist: 'The Beatles',
-      title: 'Abbey Road',
-      description:
-        'Yellow Submarine is the tenth studio album by English rock band the Beatles, released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.',
-      year: 1969,
-      image:
-        'https://upload.wikimedia.org/wikipedia/en/thumb/4/42/Beatles_-_Abbey_Road.jpg/220px-Beatles_-_Abbey_Road.jpg',
-    },
-    {
-      artistId: 2,
-      artist: 'The Beatles',
-      title: 'Let It Be',
-      description:
-        "Let It Be is the twelfth and final studio album by the English rock band the Beatles. It was released on 8 May 1970, almost a month after the group's break-up.",
-      year: 1970,
-      image:
-        'https://upload.wikimedia.org/wikipedia/en/thumb/2/25/LetItBe.jpg/220px-LetItBe.jpg',
-    },
-  ]);
+const App = (props) => {
+  const [searchPhrase, setSearchPhrase] = useState('');
+  const [albumList, setAlbumList] = useState([]);
+
+  let refresh = false;
+
+  const updateSearchResults = (phrase) => {
+    console.log('phrase is '+phrase);
+    setSearchPhrase(phrase);
+  };
+
+  useEffect(()=> {
+    //setAlbumList(albums);
+    loadAlbums();
+  },[refresh]);
+
+  const loadAlbums = async () => {
+    const response = await dataSource.get('/albums');
+
+    setAlbumList(response.data);
+  }
 
   const renderedList = () => {
     return albumList.map((album) => {
-      return (
-        <Card
-          albumTitle={album.title}
-          albumDescription={album.description}
-          buttonText='OK'
-          imageURL={album.image}
-        />
-      );
+      if(album.description.toLowerCase().includes(searchPhrase.toLowerCase())||searchPhrase==='') {
+        return (
+          <Card
+            key={album.albumId}
+            albumTitle={album.title}
+            albumDescription={album.description}
+            buttonText='OK'
+            imageURL={album.image}
+          />
+        );
+      } else {
+        console.log('Does not match '+searchPhrase);
+        return null;
+      }
     });
   };
 
-  return <div className='container'>{renderedList()}</div>;
+  return <div>
+      <div className='container'>
+        <SearchForm onSubmit={updateSearchResults}/>
+      </div>
+      <div className='container'>{renderedList()}</div>
+    </div>;
 };
 
 export default App;
